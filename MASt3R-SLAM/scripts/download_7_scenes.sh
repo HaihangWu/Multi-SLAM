f6321a501c98b4397d518bc6967a88670af12f14
hthh1211 <hthh1211@sina.com>
2025-10-19 17:40:48 +0800

    Merge remote-tracking branch 'origin/main'

--- MASt3R-SLAM/scripts/download_7_scenes.sh
+++ MASt3R-SLAM/scripts/download_7_scenes.sh
@@ -17,18 +17,15 @@ for url in "${urls[@]}"; do
     file_name=$(basename "$url")
     #echo "Downloading $file_name..."
     #wget "$url" -O "$dest/$file_name"
-    echo "Unzipping $file_name..."
-    unzip "$dest/$file_name" -d "$dest"
+#    echo "Unzipping $file_name..."
+#    unzip "$dest/$file_name" -d "$dest"
     #unzip "$dest/${file_name%.*}/seq-01" -d "$dest/${file_name%.*}"
-    for url in "${urls[@]}"; do
-        file_name=$(basename "$url")
-        scene_name="${file_name%.*}"
-        echo "Unzipping remaining sequences in $scene_name..."
-        for seq_dir in "$dest/$scene_name"/seq-*; do
-            if [ "$(basename "$seq_dir")" != "seq-01" ]; then
-                unzip "$seq_dir"/* -d "$seq_dir"
-            fi
-        done
+    scene_name="${file_name%.*}"
+    echo "Unzipping remaining sequences in $scene_name..."
+    for seq_dir in "$dest/$scene_name"/seq-*; do
+        if [ "$(basename "$seq_dir")" != "seq-01" ]; then
+            unzip "$seq_dir"/* -d "$seq_dir"
+        fi
     done
 done
 
