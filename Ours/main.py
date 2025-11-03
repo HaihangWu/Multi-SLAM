3a195e15818c4abd4b316897c3b93938bfe96c83
hthh1211 <hthh1211@sina.com>
2025-11-03 16:45:13 +0800

    Merge remote-tracking branch 'origin/main'

--- Ours/main.py
+++ Ours/main.py
@@ -139,6 +139,7 @@ class MultiAgentSystem:
                     T_WC_tmp = global_kfs[i].T_WC.to(device_tmp)
                 else:
                     T_WC_tmp = global_kfs[i].T_WC
+                print(self.keyframes[agent_id][i - start],T_WC_tmp)
                 self.keyframes[agent_id].update_T_WCs(T_WC_tmp, i - start)
 
             # Step 5: Save results
